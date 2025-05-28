8f4cde971c8bfac85008cb854fc36adc063560fb
Yusri Ariff <yusriariff7@gmail.com>
2025-05-29 00:46:05 +0800

    feat: update default route

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/challenge_1",
+        permanent: true,
+      },
+    ];
+  },
   /* config options here */
 };
 
